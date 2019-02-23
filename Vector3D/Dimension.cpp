e9c9bf6f01d4910e3939d65f3eb0dfeb9eb7104f
raphael-vock <raphaelvock@gmail.com>
2019-02-23 15:06:02 +0100

    Update Dimension.cpp

--- Vector3D/Dimension.cpp
+++ Vector3D/Dimension.cpp
@@ -1,15 +1,18 @@
 #include "Dimension.h"
 
-Dimension::Dimension(std::string u, double epsilon){
+Dimension::Dimension(std::string u, double var_epsilon){
 	unit = u;
-	tolerance = epsilon;
+	epsilon = var_epsilon;
+}
+
+void Dimension::print(void) const{
+	std::cout << unit;
 }
 
 bool Dimension::operator==(const Dimension &dim_arg){
 	return this->unit == dim_arg.unit;
 }
 
-Dimension* A(new Dimension("",1e-3));// adimensional
 Dimension* MASS(new Dimension("kg",1e-23));
 Dimension* LENGTH(new Dimension("m",1e-10));
 Dimension* VELOCITY(new Dimension("m/s",1e-5));
