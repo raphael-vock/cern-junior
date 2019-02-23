965f0b5313efea5530f28ee15260e02eb1179575
raphael-vock <raphaelvock@gmail.com>
2019-02-23 15:06:06 +0100

    Update Vector3D.cpp

--- Vector3D/Vector3D.cpp
+++ Vector3D/Vector3D.cpp
@@ -3,9 +3,9 @@
 #include <string>
 #include "Vector3D.h"
 
-Vector3D::Vector3D(Dimension* p_dim_arg, double a = 0, double b = 0, double c = 0){// by default, new vectors are 0
-	p_dim = p_dim_arg;
+const double default_epsilon(1e-10);
 
+Vector3D::Vector3D(double a, double b, double c){// by default, new vectors are 0
 	x = a;
 	y = b;
 	z = c;
@@ -28,26 +28,44 @@ void Vector3D::print(void) const{
 }
 
 
-Vector3D Vector3D::opp(void) const{
-	Vector3D Res(A);
+Vector3D Vector3D::operator-(void) const{
+	Vector3D Res;
 	Res.setCoords(-x,-y,-z);
 	return Res;
 }
 
+void Vector3D::operator*=(const double &lambda){
+	x *= lambda;
+	y *= lambda;
+	z *= lambda;
+}
+
 Vector3D Vector3D::operator*(const double &lambda) const{
-	Vector3D Res(A);
-	Res.setCoords(lambda * x, lambda * y, lambda * z);
+	Vector3D Res(*this);
+	Res *= lambda;
 	return Res;
+}
+
+void Vector3D::operator+=(const Vector3D &v){
+	x += v.x;
+	y += v.y;
+	z += v.z;
 }
 
 Vector3D Vector3D::operator+(const Vector3D &v) const{
-	Vector3D Res(A);
-	Res.setCoords(this->x + v.x, this->y + v.y, this->z + v.z);
+	Vector3D Res(v);
+	Res += *this;
 	return Res;
 }
 
+void Vector3D::operator-=(const Vector3D &v){
+	x -= v.x;
+	y -= v.y;
+	z -= v.z;
+}
+
 Vector3D Vector3D::operator-(const Vector3D &v) const{
-	return *this + v.opp();
+	return *this + (-v);
 }
 
 double Vector3D::operator|(const Vector3D &v) const{
@@ -56,7 +74,7 @@ double Vector3D::operator|(const Vector3D &v) const{
 
 Vector3D Vector3D::operator^(const Vector3D &v) const{
 	// note that member function binary operator overloading passes "this" as first argument and the method argument as second. i.e. x^y = x.operator^(y);
-	Vector3D Res(A);
+	Vector3D Res;
 	Res.x = this->y * v.z - this->z * v.y;
 	Res.y = v.x * this->z - v.z * this->x;
 	Res.z = this->x * v.y - this->y * v.x;
@@ -73,12 +91,11 @@ double Vector3D::norm(void) const{
 }
 
 bool Vector3D::is_zero(void) const{
-	return this->norm2() <= this->p_dim->tolerance;
+	return this->norm2() <= default_epsilon;
 }
 
 bool Vector3D::operator==(const Vector3D& v) const{
-	if(not(*this->p_dim == *v.p_dim)) throw 1;
-	else return (*this - v).is_zero();
+	return (*this - v).is_zero();
 }
 
 Vector3D Vector3D::unitary(void) const{
