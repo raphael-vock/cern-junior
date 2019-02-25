9a107400b4b6b6a9d108902f7b3179a3d1fc758c
raphael-vock <raphaelvock@gmail.com>
2019-02-26 00:56:13 +0100

    Update text_viewer.cpp

--- spheres/text/text_viewer.cpp
+++ spheres/text/text_viewer.cpp
@@ -2,11 +2,9 @@
 #include "text_viewer.h"
 #include "content.h"
 
-void TextViewer::draw(Content const&){
+void TextViewer::draw(Content const& to_draw){
 	/* Dans ce premier exemple très simple, on n'utilise       *
 	 * pas l'argument Contenu. Nous ne l'avons donc pas nommé. */
 
-	stream << "A sphere" << std::endl;
-	stream << "Another sphere" << std::endl;
-	stream << "A third sphere" << std::endl;
+	stream << to_draw.getAngle() << std::endl;
 }
