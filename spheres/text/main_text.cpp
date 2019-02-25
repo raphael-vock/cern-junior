b480158c1277725c77d4da7b3e97505d0c86125c
raphael-vock <raphaelvock@gmail.com>
2019-02-26 00:56:02 +0100

    Update main_text.cpp

--- spheres/text/main_text.cpp
+++ spheres/text/main_text.cpp
@@ -6,6 +6,16 @@ using namespace std;
 int main(void){
 	TextViewer screen(cout);
 	Content c(&screen);
+
+	cout << "Initially:" << endl;
+	c.draw();
+
+	c.evolve(0.1);
+	cout << "After one step:" << endl;
+	c.draw();
+
+	c.evolve(0.1);
+	cout << "After two steps:" << endl;
 	c.draw();
 
 	return 0;
