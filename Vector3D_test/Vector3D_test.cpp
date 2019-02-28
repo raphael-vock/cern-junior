100266c34eefaa1ffc0379153fcdb20331ca7172
raphael-vock <raphaelvock@gmail.com>
2019-02-28 20:13:16 +0100

    Create Vector3D_test.cpp

---
+++ Vector3D_test/Vector3D_test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Vector3D.h"
+
+using namespace std;
+
+int main(void){
+	cout << "\n";
+
+	Vector3D u(1.0, 2.0, -0.1);
+	Vector3D v(2.6, 3.5, 4.1);
+	Vector3D w(3.9, 3.8, 3.7);
+	
+	cout << "Si :\n u = "; u.print();
+	cout << "\n v = "; v.print();
+	cout << "\n w = "; w.print();
+
+	cout << "\nAlors :";
+	cout << "\n u+v+w = "; (u+v+w).print();
+	cout << "\n u-v = "; (u-v).print();
+	cout << "\n 3.u = "; (3.0*u).print();
+	cout << "\n u.v = "; cout << (u|v);
+	cout << "\n u^v = "; (u^v).print();
+	cout << "\n |u|^2 = "; cout << u.norm2();
+	cout << "\n |u| = "; cout << u.norm();
+	cout << "\n |v|^2 = "; cout << v.norm2();
+	cout << "\n |v| = "; cout << v.norm();
+	cout << "\n unitaire(v) = "; v.unitary().print();
+	cout << "\n u.(v^w) = "; cout << mixed_prod(u,v,w);
+
+	cout << "\n\n";
+}
