9dcb5ea135d820454659ea2926e38007b36395bc
raphael-vock <raphaelvock@gmail.com>
2019-02-28 20:13:10 +0100

    Create opengl_view.h

---
+++ spheres/graphical_app/opengl_view.h
@@ -0,0 +1,42 @@
+#ifndef OPENGL_VIEW
+#define OPENGL_VIEW
+
+#include <array>
+#include <QOpenGLShaderProgram> // shaders
+#include <QMatrix4x4>
+#include "glsphere.h"
+#include "canvas.h"
+
+typedef std::array<double,3> RGB;
+
+class OpenGLView : public Canvas{
+	public:
+		// drawing methods, inherited from Canvas
+		virtual void draw(Content const& to_draw) override;
+
+		// initalization methods
+		void init();
+		void initializePosition();
+
+		// setters
+		void setProjection(QMatrix4x4 const& projection){
+			prog.setUniformValue("projection", projection);
+		}
+		void translate(double x, double y, double z);
+		void rotate(double angle, double dir_x, double dir_y, double dir_z);
+
+		// some useful methods
+		void drawAxes(QMatrix4x4 const& pov, bool is_in_color = true);
+		void drawCube(QMatrix4x4 const& pov = QMatrix4x4(), RGB color = {1.0,0.0,0.0});
+		void drawSphere(QMatrix4x4 const& point_de_vue, RGB color = {1.0,0.0,0.0});
+
+	private:
+		// OpenGL shader in Qt class
+		QOpenGLShaderProgram prog;
+		GLSphere sphere;
+
+		// Camera
+		QMatrix4x4 pov_matrix;
+};
+
+#endif
