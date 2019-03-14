fe19a08ecc12bfc1884789761303770421b28e26
raphael-vock <raphaelvock@gmail.com>
2019-03-14 11:28:11 +0100

    changed main

--- src/graphical_app/main_qt_gl.cpp
+++ src/graphical_app/main_qt_gl.cpp
@@ -1,5 +1,5 @@
-#include <QApplication>
 #include <QMainWindow>
+#include <QApplication>
 #include <random>
 #include <cmath>
 
@@ -18,7 +18,7 @@ int main(int argc, char* argv[]){
 
 	double m(0.05);// mass of asteroids
 	double r(0.015);// mass of asteroids
-	int N(5000);// number of asteroids
+	int N(5e4);// number of asteroids
 
 	// creating the sun...
 	Omega.insert(
@@ -29,7 +29,7 @@ int main(int argc, char* argv[]){
 	);
 
 	std::random_device gen;
-	std::normal_distribution<double> distance(10*R, 1.5*R);
+	std::normal_distribution<double> distance(10*R, 1.0*R);
 	std::uniform_real_distribution<double> angle(0.0, 2*M_PI);
 
 	// asteroids...
@@ -41,6 +41,14 @@ int main(int argc, char* argv[]){
 		v *= sqrt(Particle::G*M/d);
 		Omega.insert(x,v,m,0.0,r);
 	}
+	/* for(int i(1); i <= 10; ++i){ */
+	/* 	double d(15*R); */
+	/* 	double theta(angle(gen)); */
+	/* 	Vector3D x(d*sin(theta), d*cos(theta), 0.0); */
+	/* 	Vector3D v(cos(theta), -sin(theta), 0.0); */
+	/* 	v *= sqrt(Particle::G*M/d); */
+	/* 	Omega.insert(x,v,10000,0.0,1); */
+	/* } */
 
 	GLWidget w(nullptr, Omega);
 
